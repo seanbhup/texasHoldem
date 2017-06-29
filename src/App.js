059bccf313c0f794519ddb60e2438dfbc3640e64
Sean Bhupathi <seanbhupathi@gmail.com>
2017-06-29 09:27:54 -0400

    init texasholdem sanity

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PokerTable from './customComponents/PokerTable.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+          <PokerTable />
+      </div>
+
+    );
+  }
+}
+
+export default App;
---
+++ src/customComponents/PokerTable.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class PokerTable extends Component {
+    render(){
+        return(
+            <div>Poker Table here</div>
+        )
+    }
+}
+
+export default PokerTable;
